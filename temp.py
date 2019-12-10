0a700f4864520088be1c764f01b177a327b4998f
Alex Golts <alexgbiz@gmail.com>
2019-12-10 14:05:38 +0000

    day3/p1 - WIP

--- temp.py
+++ temp.py
@@ -71,7 +71,29 @@ for noun in range(100):
             
 # part 1:
 
+def calc_wire_path(wire):
+    path = [(0,0)] # x,y convention
+    for item in wire:
+        if item[0] == 'L':
+            path.append((path[-1][0]-int(item[1:]), path[-1][1]))
+        elif item[0] == 'R':
+            path.append((path[-1][0]+int(item[1:]), path[-1][1]))
+        elif item[0] == 'U':
+            path.append((path[-1][0], path[-1][1]+int(item[1:])))
+        elif item[0] == 'D':
+            path.append((path[-1][0], path[-1][1]-int(item[1:])))
+        
+    return path  
+
 f=open('input3.txt','r')
 txt=f.read()
 txt = txt.split('\n')
 txt=txt[:-1]
+wire1 = txt[0].split(',')
+wire2 = txt[1].split(',')
+
+wire1_path = calc_wire_path(wire1)
+wire2_path = calc_wire_path(wire2)
+
+inter_points = calc_inter_points(wire1_path, wire2_path)
+min_dist = closest_point(inter_points)
