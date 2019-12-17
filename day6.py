4e4e53929ac43998e61591eeaec1e5f89dc19ca2
Alex Golts <alex@golts.net>
2019-12-17 22:17:38 +0200

    day6/part2

--- day6.py
+++ day6.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 f = open('input6.txt','r')
 txt = f.read()
 txt = txt.split('\n')
@@ -24,7 +26,10 @@ def num_descendants(node):
         return cnt
         
     
+
+    
 tree_nodes = {}
+# create a tree and put all nodes in a dictionary
 for item in txt:
     if item[0] not in tree_nodes:
         tree_nodes[item[0]] = Node(item[0])
@@ -43,3 +48,58 @@ for key in tree_nodes:
 print('total = ' + str(total))
 
 
+
+# Part 2:
+
+def find_parent(node_name):
+    if node_name == 'COM':
+        return None
+    for key in tree_nodes:
+        if tree_nodes[node_name] in tree_nodes[key].children:
+            return tree_nodes[key].name
+        
+
+def is_anscestor(a, b):
+    # find whether node a is an anscestor of b
+    # and return the number of generations separating them
+    if b.name == a.name:
+        return False, 0
+    cur_node_name = find_parent(b.name)
+    if cur_node_name == None:
+        return False, np.nan
+    cur_node = tree_nodes[cur_node_name]
+    cnt=1
+    while True:
+        if cur_node.name == a.name:
+            return True, cnt
+        cur_node_name = find_parent(cur_node.name)
+        if cur_node_name == None:
+            return False, np.nan
+        cur_node = tree_nodes[cur_node_name]
+        cnt+=1
+        
+def find_common_anscestor(a, b):
+    # find closest common anscestor of node a and b
+    # and return it's distance (in generations) for each
+    found = False
+    cur_parent_name = find_parent(a.name)
+    dist_from_a = 0
+    while not found:
+        is_ans, dist_from_b = is_anscestor(tree_nodes[cur_parent_name], b)
+        if is_ans:
+            common_anscestor = tree_nodes[cur_parent_name]
+            found = True
+        cur_parent_name = find_parent(cur_parent_name)
+        dist_from_a += 1
+    return common_anscestor, dist_from_a, dist_from_b
+
+
+        
+you_parent = find_parent('YOU')
+san_parent = find_parent('SAN')
+
+#find_shortest_path(you_parent, san_parent)
+
+_,dist1,dist2=find_common_anscestor(tree_nodes[you_parent], tree_nodes[san_parent])
+
+print('shortest path = ' + str(dist1+dist2))
