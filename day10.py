5a7659cdb8dd9fd8a36fe7302f58070d65c06e36
Alex Golts <alex@golts.net>
2020-01-03 15:26:02 +0200

    day10/part2

--- day10.py
+++ day10.py
@@ -7,8 +7,31 @@ txt = txt.split('\n')
 txt = txt[:-1]
 
 
+# txt = ['.#..##.###...#######',
+#        '##.############..##.',
+#        '.#.######.########.#',
+#        '.###.#######.####.#.',
+#        '#####.##.#.##.###.##',
+#        '..#####..#.#########',
+#        '####################',
+#        '#.####....###.#.#.##',
+#        '##.#################',
+#        '#####.##.###..####..',
+#        '..######..##.#######',
+#        '####.##.####...##..#',
+#        '.#####..#.######.###',
+#        '##...#.##########...',
+#        '#.##########.#######',
+#        '.####.#.###.###.#.##',
+#        '....##.##.###..#####',
+#        '.#.#.###########.###',
+#        '#.#.#.#####.####.###',
+#        '###.##.####.##.#..##']
+
 rows = len(txt)
 cols = len(txt[0])
+
+# Note: I use point = (row,col) or (y,x) convention
 
 def euclidean_distance(p1, p2):
     return np.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
@@ -57,17 +80,81 @@ for r in range(rows):
         if txt[r][c] == '#':
             points.append((r,c))
 
-# matrix denoting visibility from each point to other points:
-visibility_mat = np.zeros((len(points), len(points)))
-for r in range(len(points)):
-    for c in range(len(points)):
-        if r==c:
-            continue
-        visibility_mat[r,c] = is_visible(points[r], points[c], points)
+def calc_visibility_mat(points):
+    # matrix denoting visibility from each point to other points:
+    visibility_mat = np.zeros((len(points), len(points)))
+    for r in range(len(points)):
+        for c in range(len(points)):
+            if r==c:
+                continue
+            visibility_mat[r,c] = is_visible(points[r], points[c], points)
+    return visibility_mat
 
+print('Part 1 - calculating visibility matrix (this takes a while...)')
+visibility_mat = calc_visibility_mat(points)
 best_idx = np.argmax(np.sum(visibility_mat,0))
 best_num_visible = int(np.max(np.sum(visibility_mat,0)))
 best_point = points[best_idx]
 print('Part 1 answer:')
 print('best location = ' + str(best_point))        
 print('# of points visible = ' + str(best_num_visible))
+
+
+# Part 2
+
+start_point = best_point
+
+def angle_to_point(start_point, p):
+    # find the angle between the vector from start point
+    # to point p, and an upward facing vector (1,0)
+    p = np.array(p)
+    sp = np.array(start_point)
+    up = np.array((-1,0))
+    sp_to_p = p - sp
+
+    angle = (180/np.pi)*np.arccos(np.dot(up,sp_to_p)/(np.linalg.norm(up)*np.linalg.norm(sp_to_p)))
+    if p[1]<start_point[1]:
+        angle = 360-angle
+    return angle
+    
+# remove start point from points:
+points.remove(start_point)
+
+# calculate all angles and ranges to different points
+angles = []
+ranges = []
+for p in points:
+    angles.append(angle_to_point(start_point, p))
+    ranges.append(euclidean_distance(start_point, p))
+
+ang_sort_inds = np.argsort(angles).tolist()
+sorted_data = [[points[i], angles[i], ranges[i]] for i in ang_sort_inds]
+sorted_data_updated = sorted_data.copy()
+
+cnt = 1
+while len(sorted_data_updated)>1:
+    data_current = sorted_data_updated
+    last_point = data_current[0][0]
+    last_angle = data_current[0][1]
+    min_dist = data_current[0][2]
+    data_current = data_current[1:]
+    for i,p in enumerate(data_current):
+        if not math.isclose(data_current[i][1], last_angle):
+            sorted_data_updated.remove([last_point, last_angle, min_dist])
+            if cnt==200:
+                print(f'the {cnt}th asteroid to be vaporized is at x={last_point[1]},y={last_point[0]}')
+                print(f'part 2 answer = {last_point[1]*100+last_point[0]}')
+            cnt+=1
+            last_point = data_current[i][0]
+            last_angle = data_current[i][1]
+            min_dist = data_current[i][2]
+            #ind_to_remove = i
+        else:
+            if data_current[i][2]<min_dist:
+                last_point = data_current[i][0]
+                last_angle = data_current[i][1]
+                min_dist = data_current[i][2]
+
+        
+            
+        
