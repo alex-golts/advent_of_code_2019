b3df6c5fd2e61ed2362c97c6b240bf9e1c8d36a7
Alex Golts <alex@golts.net>
2020-04-26 22:11:33 +0300

    day17/part2

--- day17.py
+++ day17.py
@@ -1,21 +1,31 @@
 import numpy as np
 from scipy.ndimage.filters import convolve
+from copy import deepcopy
+
+def ascii2str(lst):
+    res = ''
+    for item in lst:
+        res += chr(item)
+    return res
 
 # intcode computer function - from Day 9 
-def intcode_computer(input_val, program):
+def intcode_computer(input_command, program):
     halt = False
     cnt = 0
     output_vals = []
     relative_base = 0
+    cnt_inputs=0
     while halt == False:
         optcode = program[cnt]
         if optcode%10 == 3:
             pointer_increase = 2
-            
+            input_val = input_command[cnt_inputs]
             if str(optcode).zfill(2)[0] == '2':
                 program[program[cnt+1] + relative_base] = input_val
             else:
                 program[program[cnt+1]] = input_val
+            cnt_inputs+=1
+            #print(cnt_inputs)
             
         elif optcode%10 == 4:
             pointer_increase = 2
@@ -150,6 +160,14 @@ def draw_map(prog):
             cur_line += '#'
         elif item == 46:
             cur_line += '.'
+        elif item == 60:
+            cur_line += '<'
+        elif item == 62:
+            cur_line += '>'
+        elif item == 94:
+            cur_line += '^'
+        elif item == 118:
+            cur_line += 'v'
         elif item == 10:
             out_map.append(cur_line)
             cur_line = ''
@@ -161,12 +179,9 @@ def map2img(out_map):
     img = np.zeros((len(out_map), len(out_map[0])))
     for i in range(len(out_map)):
         for j in range(len(out_map[0])):
-            try:
-                if out_map[i][j] == '#':
-                    img[i,j] = 1
-                elif out_map[i][j] == '.':
-                    img[i,j] = 0
-            except: # some lines contain fewer characters
+            if out_map[i][j] == '#':
+                img[i,j] = 1
+            else:
                 img[i,j] = 0
     return img
 
@@ -182,7 +197,7 @@ puzzle_input = [int(a) for a in txt]
 puzzle_input += [0 for i in range(100000)]
 
 # part 1:
-output, program = intcode_computer(1, puzzle_input)
+output, program = intcode_computer([1], deepcopy(puzzle_input))
 #print('part 1 output = ' + str(output))
 
 out_map = draw_map(output)
@@ -209,3 +224,31 @@ out_img = convolve(bin_img, filt)
 xx,yy = np.where(out_img==1)
 
 print('part 1 answer = ' + str(np.sum(xx*yy)))
+
+# part 2:
+puzzle_input[0]=2
+
+def str2ascii(cmd):
+    res = []
+    for c in cmd:
+        res.append(ord(c))
+    return res
+
+# I solved it manually by drawing the full path and discovering repeating patterns
+# probably not as intended but this was my last puzzle and I'm tired...
+
+full_path = 'L,12,R,4,R,4,R,12,R,4,L,12,R,12,R,4,L,12,R,12,R,4,L,6,L,8,L,8,R,12,R,4,L,6,L,8,L,8,L,12,R,4,R,4,L,12,R,4,R,4,R,12,R,4,L,12,R,12,R,4,L,12,R,12,R,4,L,6,L,8,L,8'
+
+main_prog = 'A,B,B,C,C,A,A,B,B,C\n'
+A_func = 'L,12,R,4,R,4\n'
+B_func = 'R,12,R,4,L,12\n'
+C_func = 'R,12,R,4,L,6,L,8,L,8\n'
+yes_no = 'n\n'
+
+cmd = main_prog + A_func + B_func + C_func + yes_no
+
+inp = str2ascii(cmd)
+
+output, program = intcode_computer(inp, deepcopy(puzzle_input))
+
+print(f'part 2 answer = {output[-1]}')
